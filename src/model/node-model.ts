d31c4af02e088c61db3ae709153c1c34721ef8a9
qaqmty <miaotianyugo@163.com>
2024-04-25 16:02:43 +0800

    feat: add element states

--- src/model/node-model.ts
+++ src/model/node-model.ts
@@ -1,52 +1,108 @@
+import { merge, omit, set } from "lodash";
 import {
-  t,
-  SnapshotIn,
-  Instance,
-  IAnyModelType,
+  cast,
   getRoot,
+  IAnyModelType,
+  IAnyType,
+  Instance,
+  SnapshotIn,
+  t,
+  unprotect,
 } from "mobx-state-tree";
 import { IStoreInstance } from "./store";
-import { set } from "lodash";
+
+const commonPropertiesDeclarationBetweenModelAndState = {
+  // can not use type define sugar syntax
+  name: t.optional(t.string, ""),
+  /**
+   * data bindings map: key is 'bind target', value is 'bind source'
+   * @example { "name", "elements[0].name" };
+   */
+  bindings: t.maybe(t.map(t.string)),
+};
+
+type IStateDataModelProperties =
+  typeof commonPropertiesDeclarationBetweenModelAndState & {
+    [key: string]: IAnyType;
+  };
+const StateDataModel = t.model<IStateDataModelProperties>("StateDataModel", {
+  ...commonPropertiesDeclarationBetweenModelAndState,
+});
+
+interface IStatesModelProperties {
+  [state: string]: typeof StateDataModel;
+}
+const StatesModel = t.model<IStatesModelProperties>("StatesModel");
 
 export const NodeModel = t
   .model("Node", {
     id: t.identifier,
     type: "none",
-    name: "",
+    ...commonPropertiesDeclarationBetweenModelAndState,
     /**
-     * data bindings map: key is 'bind target', value is 'bind source'
-     * @example { "name", "elements[0].name" };
+     * element state, state data contains editable property of model instance.
      */
-    bindings: t.maybe(t.map(t.string)),
+    currentState: "",
+    states: t.optional(
+      t.late(() => StatesModel),
+      {}
+    ),
   })
   .views((self) => ({
+    get currentStateData() {
+      return self.states[self.currentState];
+    },
+  }))
+  .views((self) => ({
     get processedSelf() {
-      if (!self.bindings) {
+      if (!self.currentState && !self.bindings) {
         return self;
       }
 
-      const state = (getRoot(self) as IStoreInstance).bindableState;
-      const selfCopy = { ...self };
-      for (const [targetPath, sourcePath] of self.bindings.entries()) {
-        set(selfCopy, targetPath, state.getPathValue(sourcePath));
+      // merge state deeply
+      const selfCopy = merge({}, self, self.currentStateData);
+
+      console.log("self copy", selfCopy);
+      if (!selfCopy.bindings) {
+        return selfCopy;
+      }
+
+      // resolve bindings
+      const bindableData = (getRoot(self) as IStoreInstance).bindableData;
+      for (const [targetPath, sourcePath] of selfCopy.bindings.entries()) {
+        set(selfCopy, targetPath, bindableData.getPathValue(sourcePath));
       }
       return selfCopy;
     },
   }))
   .actions((self) => ({
     set(data: SnapshotIn<IAnyModelType>) {
-      Object.assign(self, data);
+      const target = self.currentStateData ?? self;
+      // state data can not has "currentState" "states" prop
+      Object.assign(target, omit(data, ["currentState", "states"]));
     },
   }))
   .actions((self) => ({
+    // bindings
     setBindings(key: string, value: string) {
-      if (!self.bindings) {
-        self.set({ bindings: {} });
+      const target = self.currentStateData ?? self;
+      if (!target.bindings) {
+        target.bindings = cast({});
       }
-      self.bindings?.set(key, value);
+      target.bindings?.set(key, value);
     },
     unsetBindings(key: string) {
       self.bindings?.delete(key);
+    },
+
+    // states
+    createState(name: string) {
+      const stateData = StateDataModel.create();
+      unprotect(stateData);
+      self.states[name] = stateData;
+    },
+    setCurrentState(name: string) {
+      self.currentState = name;
     },
   }));
 
